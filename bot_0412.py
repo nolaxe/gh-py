31f06e59970fe537d1890b8aa3f09ca47d200af0
nolaxe <54135136+nolaxe@users.noreply.github.com>
2022-04-12 02:22:29 +0500

    Update bot_0412.py

--- bot_0412.py
+++ bot_0412.py
@@ -15,8 +15,8 @@ bot = telebot.TeleBot(telegram_token) # bot
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	#sti = open('static/welcome.webp', 'rb')
-	bot.send_sticker(message.chat.id, sti)
+	# sti = open('static/welcome.webp', 'rb')
+	# bot.send_sticker(message.chat.id, sti)
   # keyboard
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	item1 = types.KeyboardButton("🎲 Random number")
